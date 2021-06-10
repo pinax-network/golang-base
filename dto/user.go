2390efce253491dc19b0ba52f34093434ae8d077
Frederik Schöll <fschoell@users.noreply.github.com>
2021-06-10 17:27:42 +0200

    allow serialization of public user information

--- dto/user.go
+++ dto/user.go
@@ -4,8 +4,8 @@ import "time"
 
 type User struct {
 	ID          int       `json:"-"`
-	EosnID      string    `json:"-" extensions:"x-order=0"`
+	EosnID      string    `json:"eosn_id" extensions:"x-order=0"`
 	Permissions []string  `json:"-"`
-	CreatedAt   time.Time `json:"-"`
-	UpdatedAt   time.Time `json:"-"`
+	CreatedAt   time.Time `json:"created_at" extensions:"x-order=1"`
+	UpdatedAt   time.Time `json:"udpated_at" extensions:"x-order=2"`
 }
