fec9c6b71257ef27df2a135511804c8850570d24
Frederik Schöll <fschoell@users.noreply.github.com>
2021-06-10 15:25:04 +0200

    update user dto, add usermeta to protobuf

--- dto/user.go
+++ dto/user.go
@@ -1,7 +1,11 @@
 package dto
 
+import "time"
+
 type User struct {
-	ID          int      `json:"-"`
-	EosnID      string   `json:"-" extensions:"x-order=0"`
-	Permissions []string `json:"-"`
+	ID          int       `json:"-"`
+	EosnID      string    `json:"-" extensions:"x-order=0"`
+	Permissions []string  `json:"-"`
+	CreatedAt   time.Time `json:"-"`
+	UpdatedAt   time.Time `json:"-"`
 }
