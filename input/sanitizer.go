11cf7435fc9dbe45f3ee90fff4bf3e432663ee80
Frederik Schöll <fschoell@users.noreply.github.com>
2021-10-22 13:29:18 +0200

    add optional whitespace cleaning to html sanitizer, rework options

--- input/sanitizer.go
+++ input/sanitizer.go
@@ -4,6 +4,7 @@ import (
 	"github.com/microcosm-cc/bluemonday"
 	"github.com/volatiletech/null/v8"
 	"html"
+	"strings"
 )
 
 var (
@@ -11,22 +12,59 @@ var (
 	htmlSanitizer   = bluemonday.UGCPolicy()
 )
 
-func SanitizeString(input string, strict bool) string {
-	if strict {
-		return html.UnescapeString(strictSanitizer.Sanitize(input))
-	}
-	return htmlSanitizer.Sanitize(input)
+type SanitizeOptions struct {
+	Strict           bool
+	UnescapeString   bool
+	CleanWhitespaces bool
 }
 
-func SanitizeNullString(input null.String, strict bool) null.String {
+func GetDefaultStrictOptions() SanitizeOptions {
+	return SanitizeOptions{
+		Strict:           true,
+		UnescapeString:   true,
+		CleanWhitespaces: true,
+	}
+}
+
+func GetDefaultHtmlOptions() SanitizeOptions {
+	return SanitizeOptions{
+		Strict:           false,
+		UnescapeString:   false,
+		CleanWhitespaces: true,
+	}
+}
+
+// SanitizeString will apply a html sanitization on the given string. If strict is set to true all html tags will be
+// removed (bluemonday.StrictPolicy() will be applied). Otherwise it will only remove html tags and attributes that are
+// deemed dangerous (bluemonday.UGCPolicy() will be applied).
+func SanitizeString(input string, options SanitizeOptions) string {
+
+	res := input
+
+	if options.Strict {
+		res = strictSanitizer.Sanitize(res)
+	} else {
+		res = htmlSanitizer.Sanitize(res)
+	}
+
+	if options.UnescapeString {
+		res = html.UnescapeString(res)
+	}
+
+	if options.CleanWhitespaces {
+		res = strings.TrimSpace(res)
+	}
+
+	return res
+}
+
+// SanitizeNullString provides the functionality of SanitizeString for the null.String datatype. If the input
+// is not valid, it will return the input variable. Otherwise it returns the result of SanitizeString as null.String
+func SanitizeNullString(input null.String, options SanitizeOptions) null.String {
 
 	if !input.Valid {
-		return null.StringFromPtr(nil)
+		return input
 	}
 
-	if strict {
-		return null.StringFrom(html.UnescapeString(strictSanitizer.Sanitize(input.String)))
-	} else {
-		return null.StringFrom(htmlSanitizer.Sanitize(input.String))
-	}
+	return null.StringFrom(SanitizeString(input.String, options))
 }
