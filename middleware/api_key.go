38fe778171adbcad59efa26a523321a67c569f84
Frederik Schöll <fschoell@users.noreply.github.com>
2022-04-27 11:00:43 +0200

    make header configurable for the api key middleware

--- middleware/api_key.go
+++ middleware/api_key.go
@@ -9,22 +9,27 @@ import (
 )
 
 type ApiKeyMiddleware struct {
-	apiKey string
+	apiKey       string
+	apiKeyHeader string
 }
 
-func NewApiKeyMiddleware(apiKey string) (*ApiKeyMiddleware, error) {
+func NewApiKeyMiddleware(apiKey, apiKeyHeader string) (*ApiKeyMiddleware, error) {
 
 	if apiKey == "" || len(apiKey) < 16 {
 		return nil, errors.New("invalid or empty api key given")
 	}
 
-	return &ApiKeyMiddleware{apiKey: apiKey}, nil
+	if apiKeyHeader == "" {
+		return nil, errors.New("api key header is empty")
+	}
+
+	return &ApiKeyMiddleware{apiKey: apiKey, apiKeyHeader: apiKeyHeader}, nil
 }
 
 func (a *ApiKeyMiddleware) Authenticate() gin.HandlerFunc {
 	return func(c *gin.Context) {
 
-		apiKey := c.GetHeader("X-API-KEY")
+		apiKey := c.GetHeader(a.apiKeyHeader)
 
 		if apiKey == "" {
 			helper.ReportPublicErrorAndAbort(c, response.Unauthorized, "no api key set")
