11409d9c11f2f92e6dde93f3140c4046c8f0077b
Frederik Schöll <fschoell@users.noreply.github.com>
2022-05-16 17:13:09 +0200

    add error messages for creating bounties

--- response/errors.go
+++ response/errors.go
@@ -25,27 +25,30 @@ const (
 
 	FORBIDDEN = "forbidden" // not allowed to access this endpoint
 
-	NOT_FOUND_ABUSE_REPORT       = "abuse_report_not_found"       // the requested abuse report was not found
-	NOT_FOUND_FILE               = "file_not_found"               // the requested file was not found
-	NOT_FOUND_FUNDING_ADDRESS    = "funding_address_not_found"    // the eos account for the funding address could not be found on chain
-	NOT_FOUND_GRANT              = "grant_not_found"              // the requested grant was not found
-	NOT_FOUND_GRANT_CATEGORY     = "grant_category_not_found"     // the requested grant category was not found
-	NOT_FOUND_GRANT_TRANSLATION  = "grant_translation_not_found"  // the requested grant translation was not found
-	NOT_FOUND_KYC_REQUIRED       = "kyc_not_required"             // kyc information has been requested but kyc is not required for the given user
-	NOT_FOUND_LANGUAGE           = "language_not_found"           // the requested language was not found
-	NOT_FOUND_LINKED_ACCOUNT     = "linked_account_not_found"     // the given account name is not linked to an eosn id
-	NOT_FOUND_LINKED_SOCIAL      = "linked_social_not_found"      // the user tried to unlink a social that hasn't been linked before
-	NOT_FOUND_MATCHING_PARTNER   = "matching_partner_not_found"   // the requested matching partner was not found
-	NOT_FOUND_MATCHING_ROUND     = "matching_round_not_found"     // the requested matching round was not found
-	NOT_FOUND_PORT_ACCOUNT       = "port_account_not_found"       // there has been no port verification found for the authenticated eosn_id
-	NOT_FOUND_REGION             = "region_not_found"             // the requested region was not found
-	NOT_FOUND_RESOURCE           = "resource_not_found"           // the requested resource was not found
-	NOT_FOUND_ROUTE              = "route_not_found"              // the requested route was not found
-	NOT_FOUND_SEASON             = "season_not_found"             // the requested season was not found
-	NOT_FOUND_TOKEN              = "token_not_found"              // the requested token was not found
-	NOT_FOUND_USER               = "user_not_found"               // the requested user was not found
-	NOT_FOUND_USER_CHAIN_ACCOUNT = "user_chain_account_not_found" // there is no eosn chain account for the given eosn id
-	NOT_FOUND_USER_PROFILE       = "user_profile_not_found"       // the requested user profile was not found
+	NOT_FOUND_ABUSE_REPORT            = "abuse_report_not_found"            // the requested abuse report was not found
+	NOT_FOUND_BOUNTY_CATEGORY         = "bounty_category_not_found"         // the requested bounty category was not found
+	NOT_FOUND_BOUNTY_SKILL            = "bounty_skill_not_found"            // the requested bounty skill was not found
+	NOT_FOUND_BOUNTY_EXPERIENCE_LEVEL = "bounty_experience_level_not_found" // the requested bounty experience level was not found
+	NOT_FOUND_FILE                    = "file_not_found"                    // the requested file was not found
+	NOT_FOUND_FUNDING_ADDRESS         = "funding_address_not_found"         // the eos account for the funding address could not be found on chain
+	NOT_FOUND_GRANT                   = "grant_not_found"                   // the requested grant was not found
+	NOT_FOUND_GRANT_CATEGORY          = "grant_category_not_found"          // the requested grant category was not found
+	NOT_FOUND_GRANT_TRANSLATION       = "grant_translation_not_found"       // the requested grant translation was not found
+	NOT_FOUND_KYC_REQUIRED            = "kyc_not_required"                  // kyc information has been requested but kyc is not required for the given user
+	NOT_FOUND_LANGUAGE                = "language_not_found"                // the requested language was not found
+	NOT_FOUND_LINKED_ACCOUNT          = "linked_account_not_found"          // the given account name is not linked to an eosn id
+	NOT_FOUND_LINKED_SOCIAL           = "linked_social_not_found"           // the user tried to unlink a social that hasn't been linked before
+	NOT_FOUND_MATCHING_PARTNER        = "matching_partner_not_found"        // the requested matching partner was not found
+	NOT_FOUND_MATCHING_ROUND          = "matching_round_not_found"          // the requested matching round was not found
+	NOT_FOUND_PORT_ACCOUNT            = "port_account_not_found"            // there has been no port verification found for the authenticated eosn_id
+	NOT_FOUND_REGION                  = "region_not_found"                  // the requested region was not found
+	NOT_FOUND_RESOURCE                = "resource_not_found"                // the requested resource was not found
+	NOT_FOUND_ROUTE                   = "route_not_found"                   // the requested route was not found
+	NOT_FOUND_SEASON                  = "season_not_found"                  // the requested season was not found
+	NOT_FOUND_TOKEN                   = "token_not_found"                   // the requested token was not found
+	NOT_FOUND_USER                    = "user_not_found"                    // the requested user was not found
+	NOT_FOUND_USER_CHAIN_ACCOUNT      = "user_chain_account_not_found"      // there is no eosn chain account for the given eosn id
+	NOT_FOUND_USER_PROFILE            = "user_profile_not_found"            // the requested user profile was not found
 
 	METHOD_NOT_ALLOWED = "method_not_allowed" // http method is not allowed on this endpoint
 
@@ -62,6 +65,7 @@ const (
 	CONFLICT_MAX_ACCOUNTS_LINKED           = "max_accounts_linked"               // the maximum amount of accounts has already been linked
 	CONFLICT_MAX_PENDING_GRANTS            = "max_pending_grants_reached"        // the maximum of pending grants is reached for this user
 	CONFLICT_MAX_TOTAL_GRANTS              = "max_total_grants_reached"          // the maximum amount of grants is reached for this user
+	CONFLICT_MIN_REWARD_NOT_REACHED        = "min_reward_amount_not_reached"     // the minimum reward amount for a bounty has not been reached
 	CONFLICT_NOT_IN_ACTIVE_MATCHING_ROUND  = "not_in_active_matching_round"      // the grant is not in an active matching round
 	CONFLICT_PORT_VERIFICATION_EXPIRED     = "port_verification_expired"         // the port verification has already been expired
 	CONFLICT_PORT_ATTESTATION_INSUFFICIENT = "port_attestation_insufficient"     // a port verification has been found but the attestation level is insufficient
