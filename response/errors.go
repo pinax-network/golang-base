4717da060b949ae41ddd6becf2c153f4e99e4ea0
Frederik Schöll <fschoell@users.noreply.github.com>
2022-10-26 12:28:14 +0200

    add NOT_FOUND_BOUNTY_APPLICATION

--- response/errors.go
+++ response/errors.go
@@ -27,6 +27,7 @@ const (
 
 	NOT_FOUND_ABUSE_REPORT            = "abuse_report_not_found"            // the requested abuse report was not found
 	NOT_FOUND_BOUNTY                  = "bounty_not_found"                  // the requested bounty was not found
+	NOT_FOUND_BOUNTY_APPLICATION      = "bounty_application_not_found"      // the requested bounty application was not found
 	NOT_FOUND_BOUNTY_CATEGORY         = "bounty_category_not_found"         // the requested bounty category was not found
 	NOT_FOUND_BOUNTY_SKILL            = "bounty_skill_not_found"            // the requested bounty skill was not found
 	NOT_FOUND_BOUNTY_EXPERIENCE_LEVEL = "bounty_experience_level_not_found" // the requested bounty experience level was not found
