def47ede237c9953d37ad804f59393e464fe7514
Frederik Schöll <fschoell@posteo.de>
2023-10-13 16:25:54 +0200

    rename parameter

--- service/user.go
+++ service/user.go
@@ -11,5 +11,5 @@ type UserService interface {
 	// ID (for example, the eosn_id or uuid).
 	// This method might panic if no user is found, create a new one or return the response.ApiError in case
 	// a specific error should be returned when trying to extract that user from the authentication middleware.
-	ExtractUserByGUID(ctx context.Context, eosnId string) (*base_models.User, *response.ApiError)
+	ExtractUserByGUID(ctx context.Context, guid string) (*base_models.User, *response.ApiError)
 }
