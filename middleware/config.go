fdfc5817a00e200b2fc9073ce6f7e75c25b3047a
Frederik Schöll <fschoell@users.noreply.github.com>
2025-03-04 18:35:50 +0100

    allow loading jwks keys from file instead of the url (#88)

--- middleware/config.go
+++ middleware/config.go
@@ -4,4 +4,5 @@ type JwtMiddlewareConfig struct {
 	Auth0Domain           string   `yaml:"auth0_domain" json:"auth0_domain" mapstructure:"auth0_domain" validate:"required"`                                  // Auth0 domain without protocol and trailing slash, for example login-dev.eosnation.io
 	Auth0AllowedAudiences []string `yaml:"auth0_allowed_audiences" json:"auth0_allowed_audiences" mapstructure:"auth0_allowed_audiences" validate:"required"` // Audiences allowed accessing this API
 	Namespace             string   `yaml:"namespace" json:"namespace" mapstructure:"namespace" validate:"required"`                                           // The namespace for the token claims
+	JwksFile              string   `yaml:"jwks_file" json:"jwks_file" mapstructure:"jwks_file" validate:"omitempty"`
 }
